df6fbceae6adc1ca76339047095d91053669ee8c
Teerachai <teerachai@perfenterprise.com>
2016-09-22 08:40:10 +0700

    Add BankAService.csproj in Lab4Orchestration.sln.
    Signed-off-by: Teerachai <teerachai@perfenterprise.com>

---
+++ Lab4Orchestration/BankAService/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BankAService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class BankAService : IBankAService
+    {
+        public static double Balance = InitialBalance;
+
+        private const double InitialBalance = 20000;
+
+        public double Withdraw(double amount)
+        {
+            Console.WriteLine("Requesting withdraw {0}.", amount);
+            if (Balance >= amount)
+            {
+                Console.WriteLine("Withdraw granted");
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Withdraw rejected");
+            }
+            Console.WriteLine("Balance after withdraw: {0}.", amount);
+            return amount;
+        }
+    }
+}
