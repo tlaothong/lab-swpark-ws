b5a6f5d4e1a6630d6551cd4d585e7ab47c43083f
Teerachai <teerachai@perfenterprise.com>
2016-09-22 12:43:58 +0700

    Add PersonService.svc for demonstrating a KnownType to the hierarchical structure.
    Signed-off-by: Teerachai <teerachai@perfenterprise.com>

---
+++ DemoDesignContract/DemoOnlineShop/Services/IPersonService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DemoOnlineShop.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPersonService" in both code and config file together.
+    [ServiceContract]
+    public interface IPersonService
+    {
+        [OperationContract]
+        IEnumerable<Person> GetPersons();
+        [OperationContract]
+        IEnumerable<SalesPerson> GetSalesPerson();
+    }
+
+    [DataContract]
+    [KnownType(typeof(SalesPerson))]
+    public class Person
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class SalesPerson : Person
+    {
+        public double Commission { get; set; }
+    }
+}
---
+++ DemoDesignContract/DemoOnlineShop/Services/PersonService.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DemoOnlineShop.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PersonService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PersonService.svc or PersonService.svc.cs at the Solution Explorer and start debugging.
+    public class PersonService : IPersonService
+    {
+        public void DoWork()
+        {
+        }
+
+        public IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>()
+            {
+                new Person
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                },
+                new Person
+                {
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                },
+                new SalesPerson
+                {
+                    FirstName = "Tom",
+                    LastName = "Hanks",
+                    Commission = 500,
+                },
+            };
+        }
+
+        public IEnumerable<SalesPerson> GetSalesPerson()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
